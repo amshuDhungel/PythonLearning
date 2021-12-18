674dbadbc926915f87a07e42e6dcdbf32c93bfb3
amshuDhungel <dhungelamshu05@gmail.com>
2021-12-18 18:18:08 +0545

    "Changed the source code of binary"

--- binarysearch.py
+++ binarysearch.py
@@ -1,25 +1,28 @@
 
 
-def search(list, n):
-    l = 0
-    u = len(list)- 1
+# def search(list, n):
+#     l = 0
+#     u = len(list)- 1
 
-    while l <= u:
-        mid = (l+n) // 2
-        if list(mid) == n:
-            globals()['pos']= mid
-            return True
-        else:
-            if list(mid)<n:
-                l = mid
-            else:
-                u = mid
-list = [1,3,23,34,5,6]
-list.sort()
-n = 23
-result = search(list,  n)
+#     while l <= u:
+#         mid = (l+n) // 2
+#         if list(mid) == n:
+#             globals()['pos']= mid
+#             return True
+#         else:
+#             if list(mid)<n:
+#                 l = mid
+#             else:
+#                 u = mid
+# list = [1,3,23,34,5,6]
+# list.sort()
+# n = 23
+# result = search(list,  n)
 
-if result != -1:
-    print("your number is found at", str(result))
-else:
-    print("Not found")
+# if result != -1:
+#     print("your number is found at", str(result))
+# else:
+#     print("Not found")
+
+
+# This is commenting
